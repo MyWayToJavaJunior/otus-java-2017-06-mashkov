97c798327d3c4351b2e201c81d35304a12daa878
slonikmak <slonikmak@gmail.com>
2017-06-22 11:26:53 +0300

    add L3 home work

--- L3Generics/src/main/java/ru/otus/l3Generics/Main.java
+++ L3Generics/src/main/java/ru/otus/l3Generics/Main.java
@@ -1,4 +1,33 @@
 package ru.otus.l3Generics;
 
+import ru.otus.l3Generics.utils.MyArrayList;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Main {
+    public static void main(String[] args) {
+        ////run tests
+        MyArrayList<String> myArrayList = new MyArrayList<>();
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("q");
+        arrayList.add("u");
+        myArrayList.addAll(arrayList);
+
+        myArrayList = new MyArrayList<>();
+        myArrayList.add("one");
+        myArrayList.add("two");
+        myArrayList.add("three");
+        ArrayList<String> dest = new ArrayList<>(3);
+        dest.add("1");
+        dest.add("2");
+        dest.add("3");
+        Collections.copy(dest, myArrayList);
+
+        myArrayList = new MyArrayList<>();
+        myArrayList.add("f");
+        myArrayList.add("a");
+        myArrayList.add("b");
+        Collections.sort(myArrayList);
+    }
 }
