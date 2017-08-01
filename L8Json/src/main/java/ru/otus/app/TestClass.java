10230ac2a545970c8a9dcefa085da52158fc7878
slonikmak <slonikmak@gmail.com>
2017-08-01 18:48:49 +0300

    update L8

--- L8Json/src/main/java/ru/otus/app/TestClass.java
+++ L8Json/src/main/java/ru/otus/app/TestClass.java
@@ -7,15 +7,15 @@ public class TestClass {
     String s = "2";
     InnerTestClass o;
     ArrayList<String> list;
-    HashMap<String, String> map;
+    HashMap<String, Long> map;
     Queue<String> queue;
 
     public TestClass(){
         list = new ArrayList<>(Arrays.asList("qwe","www"));
         o = new InnerTestClass();
         map = new HashMap<>();
-        map.put("1", "1");
-        map.put("2","2");
+        map.put("1", 23L);
+        map.put("2",32L);
         queue = new PriorityQueue<>();
         queue.add("qw");
         queue.add("qa");
