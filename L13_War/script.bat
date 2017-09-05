5506ebd8b9b7f5fe40b07f76dbc7cf35d1464a7c
slonikmak <slonikmak@gmail.com>
2017-09-05 21:12:59 +0300

    update lesson 13

--- L13_War/script.bat
+++ L13_War/script.bat
@@ -1,3 +1,3 @@
 @echo off
-start /wait "maven" mvn clean package
-copy C:\Users\User\IdeaProjects\otus-java-2017-06-mashkov\L13_War\target\root.warC:\Users\Anton\Downloads\lodgon-openmapfx-e6051aa87826\otus-java-2017-06-mashkov\L13_War\target\root.war C:\Users\Anton\Downloads\jetty-distribution-9.4.6.v20170531\jetty-distribution-9.4.6.v20170531\webapps
+start /WAIT /B "" mvn clean package
+copy C:\Users\Anton\Downloads\lodgon-openmapfx-e6051aa87826\otus-java-2017-06-mashkov\L13_War\target\root.war C:\Users\Anton\Downloads\jetty-distribution-9.4.6.v20170531\jetty-distribution-9.4.6.v20170531\webapps
